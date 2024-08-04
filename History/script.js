81160b29ec157ddc94f20cf266b11bd43380c957
shahd sayed <shahdsayed387@gmail.com>
2024-08-04 18:41:07 +0300

    updated

---
+++ History/script.js
@@ -0,0 +1,12 @@
+function toggle() {
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
+function EditToggle() {
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popupp = document.getElementById('popupp');
+    popupp.classList.toggle('active');
+} 
---
+++ Login/script.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('psw');
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+        "email": email.value,
+        "password": password.value
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch("http://164.92.244.59:3000/admin/login", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+            if (result != "Wrong Email Or Password...") {
+                alert("Login successful!"); 
+                window.location.href = "../Team/index.html";
+                localStorage.setItem("expecto Patronum", `MSP ${result["token"]}`);
+            }
+            else {
+                email.style.cssText += "border-color:red;";
+                password.style.cssText += "border-color:red;";
+            }
+            // console.log(result);
+        })
+        .catch((error) => console.error(error));
+})
